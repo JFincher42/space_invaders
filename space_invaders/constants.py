4abd7713d8503cb9460f58510b10bf1f6c7e2576
Jon Fincher <jfincher42@gmail.com>
2019-08-15 10:45:49 -0500

    Refactored sprite classes and constants

---
+++ space_invaders/constants.py
@@ -0,0 +1,15 @@
+"""Defines game constants used throughout the game
+"""
+
+# Imports
+import arcade
+
+# Game Constants
+SCALING = 6
+SCREEN_WIDTH = 180 * SCALING
+SCREEN_HEIGHT = 200 * SCALING
+SCREEN_TITLE = "Space Invaders"
+BACKGROUND_COLOR = arcade.csscolor.BLACK
+
+RIGHT_EDGE = SCREEN_WIDTH - SCALING
+LEFT_EDGE = SCALING
---
+++ space_invaders/si_sprites.py
@@ -0,0 +1,75 @@
+"""Defines sprite subclasses for use in the Space Invaders game
+"""
+
+import arcade
+from constants import SCALING
+
+class Enemy(arcade.Sprite):
+    """Displays an enemy on the screen
+    """
+
+    # Static variable to hold points for this enemy
+    points = 0
+
+    def __init__(self, texture_list, points):
+        """Creates a new enemy sprite
+
+        Arguments:
+            texture_list {List} -- Images to show as enemy moves
+            points {int} -- How many points is this enemy worth when destroyed
+        """
+        super().__init__("space_invaders/images/alien1.png", scale=SCALING)
+
+        # Start at the first frame
+        self.current_texture = 0
+        self.textures = texture_list
+        self.set_texture(self.current_texture)
+        self.points = points
+
+    def update(self):
+        """Updates the current image to display
+        """
+
+        self.current_texture += 1
+        if self.current_texture >= len(self.textures):
+            self.current_texture = 0
+        self.set_texture(self.current_texture)
+
+
+class Explosion(arcade.Sprite):
+    """Displays an explosion animation when required
+    """
+
+    # Static variable that holds all the explosion textures
+    textures = []
+    animation_repeat = 0
+
+    def __init__(self, texture_list, repeat=10):
+        super().__init__("space_invaders/images/player.png", scale=SCALING)
+
+        # Start at the first frame
+        self.current_texture = 0
+        self.textures = texture_list
+        self.set_texture(self.current_texture)
+        self.animation_repeat = repeat
+        # To count how many rotations we've done
+        self.count = 0
+
+    def update(self):
+
+        # Update to the next frame of the animation.
+        self.current_texture += 1
+        if self.current_texture < len(self.textures):
+            self.set_texture(self.current_texture)
+
+        # If we are at the last frame, check if we still have animation to do
+        # If so, start over
+        else:
+            self.count += 1
+            if self.count < self.animation_repeat:
+                self.current_texture = 0
+                self.set_texture(self.current_texture)
+
+            # If not, kill the sprite.
+            else:
+                self.kill()
